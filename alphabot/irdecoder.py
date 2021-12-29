91a5ab0e470dc1fe825e7dd6a23246f2e20851e4
Marc R. Hoffmann <hoffmann@mountainminds.com>
2021-12-29 16:23:45 +0000

    Add repeat support for IR receiver

--- alphabot/irdecoder.py
+++ alphabot/irdecoder.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""Decoder for NEC IR Signals
+"""Decoder for the NEC infrared transmission protocol
 
 Implemented according to this specification:
 https://techdocs.altium.com/display/FPGA/NEC+Infrared+Transmission+Protocol
 """
 
-_TOLERANCE = 0.2
+
+_TOLERANCE = 0.20
+
 
 class _UnexpectedPulse(Exception):
 	pass
+
 
 class _Pulse(object):
 
@@ -27,11 +30,14 @@ class _Pulse(object):
 		if not self.matches(actual):
 			raise _UnexpectedPulse()
 
-_LEADING1 = _Pulse(9.0000)
-_LEADING2 = _Pulse(4.5000)
+
+_LEADING  = _Pulse(9.0000)
+_BEGINKEY = _Pulse(4.5000)
+_REPEAT   = _Pulse(2.2500)
 _SPACE    = _Pulse(0.5625)
 _BIT0     = _Pulse(0.5625)
 _BIT1     = _Pulse(1.6875)
+_FINAL    = _Pulse(0.5625)
 
 
 class IRDecoder(object):
@@ -68,13 +74,16 @@ class IRDecoder(object):
 	def _protocol(self):
 		while True:
 			try:
-				_LEADING1.expect((yield))
-				_LEADING2.expect((yield))
-				address = (yield from self._readbyte_with_complement())
-				command = (yield from self._readbyte_with_complement())
-				_SPACE.expect((yield))
-				
-				print("address %s, command %s" % (address, command))
-				
+				_LEADING.expect((yield))
+				duration = (yield)
+				if _BEGINKEY.matches(duration):
+					address = (yield from self._readbyte_with_complement())
+					command = (yield from self._readbyte_with_complement())
+					_FINAL.expect((yield))
+					print("address %s, command %s" % (address, command))
+				elif _REPEAT.matches(duration):
+					_FINAL.expect((yield))
+					print("repeat")
+									
 			except _UnexpectedPulse:
 				pass
